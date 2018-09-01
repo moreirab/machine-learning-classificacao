1b7eabf86e5192236eba3b2b3ae7576ecc6ffef9
Bruno Moreira dos Santos <bmds.dev@gmail.com>
2018-09-01 08:20:35 -0300

    Obtendo a taxa de acerto

--- classificacao/classifica_acessos.py
+++ classificacao/classifica_acessos.py
@@ -5,4 +5,13 @@ X,Y = carregar_acessos()
 from sklearn.naive_bayes import MultinomialNB
 modelo = MultinomialNB()
 modelo.fit(X, Y)
-print(modelo.predict([[1,0,1], [0,1,0], [1,0,0], [1,1,0], [1,1,1]]))
+
+resultado = modelo.predict(X)
+diferencas = resultado - Y
+acertos = [d for d in diferencas if d == 0]
+total_de_acertos = len(acertos)
+total_de_elementos = len(X)
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+print(taxa_de_acerto)
+print(total_de_elementos)
