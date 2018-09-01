70843a66fe15bed65eaa7a68e02b7229d68835a0
Bruno Moreira dos Santos <bmds.dev@gmail.com>
2018-09-01 08:37:24 -0300

    Treinando e testando o algoritmo

--- classificacao/classifica_acessos.py
+++ classificacao/classifica_acessos.py
@@ -1,16 +1,27 @@
 from dados import carregar_acessos
 
+# carrega todos os dados
 X,Y = carregar_acessos()
 
+# separa as primeiras 90 linhas para treinar o algoritmo
+treino_dados = X[:90]
+treino_marcacoes = Y[:90]
+
+# separa as 9 últimas linhas para testar o algoritmo
+teste_dados = X[-9:]
+teste_marcacoes = Y[-9:]
+
+# cria o modelo e treina com os dados e marcacoes que foram separadas acima
 from sklearn.naive_bayes import MultinomialNB
 modelo = MultinomialNB()
-modelo.fit(X, Y)
+modelo.fit(treino_dados, treino_marcacoes)
 
-resultado = modelo.predict(X)
-diferencas = resultado - Y
+# testa o algoritmo com os dados separados para teste
+resultado_teste = modelo.predict(teste_dados)
+diferencas = resultado_teste - teste_marcacoes
 acertos = [d for d in diferencas if d == 0]
 total_de_acertos = len(acertos)
-total_de_elementos = len(X)
+total_de_elementos = len(teste_dados)
 taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
 
 print(taxa_de_acerto)
