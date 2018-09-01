b1b4a8e533fea011fcb9964f04f8f1cbbb4501f4
Bruno Moreira dos Santos <bmds.dev@gmail.com>
2018-09-01 12:27:51 -0300

    Traduzindo as categorias com pandas

--- classificacao/src/classifica_buscas.py
+++ classificacao/src/classifica_buscas.py
@@ -1,2 +1,17 @@
-from dados import carregar_buscas
-X, Y = carregar_buscas()
+# importa a biblioteca pandas que eh utilizada para analise de dados
+import pandas
+
+# le o arquivo csv e obtem o data frame (df)
+df = pandas.read_csv('classificacao/resources/busca.csv')
+
+# por convencao, o sufixo 'df' indica que o objeto eh um data frame
+X_df = df[['home', 'busca', 'logado']]
+Y_df = df['comprou']
+
+# obtem as variaveis categoricas como variaveis binarias
+Xdummies_df = pandas.get_dummies(X_df)
+Ydummies_df = Y_df
+
+# converte o data frame em array
+X = Xdummies_df.values
+Y = Ydummies_df.values
